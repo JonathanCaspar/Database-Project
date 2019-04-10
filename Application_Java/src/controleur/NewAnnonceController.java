c50a3874aa6d5e2c8a32ba50ce7974978f86ef16
JFBlanchette <jean-francois.blanchette@umontreal.ca>
2019-04-10 15:36:53 -0400

    fix apostrophes

--- Application_Java/src/controleur/NewAnnonceController.java
+++ Application_Java/src/controleur/NewAnnonceController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
@@ -23,7 +24,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 
 public class NewAnnonceController {
-	
+
 	@FXML
 	AnchorPane annoncePane;
 	@FXML
@@ -34,29 +35,28 @@ public class NewAnnonceController {
 	private TextArea descriptionTA;
 	@FXML
 	private TextField prixTF;
-	
+
 	@FXML
 	private ComboBox categoriesCB;
-	
+
 	private float estimation = 0;
-	
+
 	private String nom, description, prix;
 	private int catID;
-	
-	
+
 	/**
 	 * Initialise la liste des categories en allant chercher les noms dans la BD
 	 */
 	@FXML
 	public void initialize() {
-		
+
 		ArrayList<String> categories = new ArrayList<String>();
 
 		QueriesItr QT = new QueriesItr("SELECT catname FROM " + DbAdapter.DB_TABLES[2] + ";");
 		ResultSet rs = QT.getResultSet();
-		
+
 		try {
-			if(rs != null) {
+			if (rs != null) {
 				while (QT.next()) {
 					categories.add(rs.getString("catname"));
 				}
@@ -65,126 +65,128 @@ public class NewAnnonceController {
 			QT.quitter();
 			e.printStackTrace();
 		}
-		
+
 		categoriesCB.getItems().addAll(categories);
-		
+
 		annoncePane.setOnKeyReleased(event -> {
-			  if (event.getCode() == KeyCode.ENTER){
-				  soumettre();
-			  }
+			if (event.getCode() == KeyCode.ENTER) {
+				soumettre();
+			}
 		});
-		
+
 	}
-	
+
 	/**
 	 * Popup de l'expert, permet de donner manuellement l'estimation du prix
 	 */
 	public void expertPopup() {
-		
+
 		TextInputDialog dialog = new TextInputDialog("");
 		dialog.setTitle("EXPERT");
 		dialog.setHeaderText("Estimation du prix du produit.");
 		dialog.setContentText("Entrez votre estimation :");
-		
+
 		Optional<String> resultat = dialog.showAndWait();
-		
-		if (resultat.isPresent()){
+
+		if (resultat.isPresent()) {
 			estimation = Float.parseFloat(resultat.get());
 		}
-		
+
 	}
+
 	/**
 	 * Popup pour que le vendeur accepte ou non l'offre
 	 */
 	public void acceptPopup() {
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Estimation par l'expert");
-		alert.setHeaderText("Votre produit a été estimé à "+ this.estimation + "$.");
+		alert.setHeaderText("Votre produit a été estimé à " + this.estimation + "$.");
 		alert.setContentText("Vous pouvez accepter ou refuser ce prix.");
 
 		ButtonType accepter = new ButtonType("Accepter", ButtonData.CANCEL_CLOSE);
-	
+
 		ButtonType refuser = new ButtonType("Refuser", ButtonData.CANCEL_CLOSE);
-		
+
 		alert.getButtonTypes().setAll(accepter, refuser);
-		
+
 		Optional<ButtonType> result = alert.showAndWait();
-		
+
 		if (result.get() == refuser) {
 			this.estimation = Float.parseFloat(this.prix);
 		}
-		
-		
+
 	}
-	
-	
-	 public boolean validForm() {
-	    	
-	    	try {
-				
-				nom = nomTF.getText();
-				description = descriptionTA.getText();
-				prix = prixTF.getText();
-				catID = categoriesCB.getSelectionModel().getSelectedIndex() + 1;
-				
-				if(nom.length() == 0 || description.length() == 0 || prix.length() == 0) {
-					errorPopup("Données manquante", "Vous n'avez pas rempli tous les champs.");
-					return false;
-				}
-				
-				if(prix.matches("^-?\\d*(\\.\\d{1,2}){0,1}$")) {
-					return true;
-				}
-				else {
-					errorPopup("Format du prix", "Le prix saisi n'est pas au bon format. \n Rappel: 10.99 .");
-					return false;
-				}
+
+	public String conversion(String texte) {
+		texte = texte.replace('\'', '\"');
 			
-			
-			}catch(NullPointerException e) {
-				
-				e.printStackTrace();
+		return texte;
+	}
+
+	public boolean validForm() {
+
+		try {
+
+			nom = conversion(nomTF.getText());
+			description = conversion(descriptionTA.getText());
+			prix = prixTF.getText();
+			catID = categoriesCB.getSelectionModel().getSelectedIndex() + 1;
+
+			if (nom.length() == 0 || description.length() == 0 || prix.length() == 0) {
 				errorPopup("Données manquante", "Vous n'avez pas rempli tous les champs.");
 				return false;
 			}
-	    	
-	    }
-	
-	
-	@FXML 
+
+			if (prix.matches("^-?\\d*(\\.\\d{1,2}){0,1}$")) {
+				return true;
+			} else {
+				errorPopup("Format du prix", "Le prix saisi n'est pas au bon format. \n Rappel: 10.99 .");
+				return false;
+			}
+
+		} catch (NullPointerException e) {
+
+			e.printStackTrace();
+			errorPopup("Données manquante", "Vous n'avez pas rempli tous les champs.");
+			return false;
+		}
+
+	}
+
+	@FXML
 	void soumettre() {
-		
-		if(validForm()) {
+
+		if (validForm()) {
 			
 			expertPopup();
 			acceptPopup();
-			
+
 			try {
-				
+
 				Statement stmt = DbAdapter.con.createStatement();
 				if (stmt != null) {
-					stmt.executeUpdate("INSERT INTO products (estimatedprice, sellingprice, sellerid, categoryid, description, name) VALUES"+
-							" ('"+ this.estimation +"', '"+ this.prix +"', "+ MainControleur.getUtilisateur()+", "+ this.catID +", '"+this.description+"','"+ this.nom+ "')");
+					stmt.executeUpdate(
+							"INSERT INTO products (estimatedprice, sellingprice, sellerid, categoryid, description, name) VALUES"
+									+ " ('" + this.estimation + "', '" + this.prix + "', "
+									+ MainControleur.getUtilisateur() + ", " + this.catID + ", '" + this.description
+									+ "','" + this.nom + "')");
 					stmt.close();
 				}
 			} catch (SQLException e) {
 				errorPopup("Probleme Base de Données", "L'insertion n'a pas pu être effectuée.");
 				e.printStackTrace();
 			}
-			
-				
-			Stage stage = (Stage) soumettre.getScene().getWindow(); 
-		    stage.close();
+
+			Stage stage = (Stage) soumettre.getScene().getWindow();
+			stage.close();
 		}
-		
+
 	}
-	
+
 	public float getEstimation() {
 		return this.estimation;
 	}
-	
-	
-	
+
 	public static void errorPopup(String typeError, String message) {
 
 		Alert alert = new Alert(AlertType.ERROR);
